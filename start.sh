f202e5b1b7d79e709db296e984416f09a0913150
Azdin Mediri <a.elmediri@gmail.com>
2024-12-07 21:57:01 +0100

    Change start.sh for choice what service start

--- start.sh
+++ start.sh
@@ -4,12 +4,18 @@ set -e
 
 # Start SSH in background
 if [ "${ENABLE_SSH:-true}" = "true" ]; then
+    echo "You choice ENABLE_SSH=true, starting SSH server..."
     service ssh start
+else
+    echo "You choice ENABLE_SSH=false, skip starting SSH server"
 fi
 
 # Start code-server in background
 if [ "${ENABLE_CODESERVER:-true}" = "true" ]; then
+    echo "You choice ENABLE_CODESERVER=true, starting code-server..."
     code-server ./src &
+else
+    echo "You choice ENABLE_CODESERVER=false, skip starting code-server"
 fi
 
 # Install all models for ComfyUI
