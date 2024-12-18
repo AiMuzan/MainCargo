e0cb1bb36bd5fab3959c199a67f420055e808c07
Azdin Mediri <a.elmediri@gmail.com>
2024-12-18 15:59:27 +0100

    ADD : Echo info

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 set -e
 
+# Get information about the container and the user
+echo "User: $(whoami)"
+echo "Home: $HOME"
+echo "Shell: $SHELL"
+echo "PWD: $PWD"
+
 service ssh start
 sleep infinity
